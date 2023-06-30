031804146a5c419ca627730f56a313fae1ed3b63
NghiaLOE <84240893+tiptopdragon0406@users.noreply.github.com>
2023-06-30 11:57:46 +0700

    Update ThanhMau.cs

--- HealthBar/ThanhMau.cs
+++ HealthBar/ThanhMau.cs
@@ -9,5 +9,7 @@ public class ThanhMau : MonoBehaviour
     public void capNhatThanhMau(float luongMauHienTai,float luongMauToiDa)
     {
         _thanhMau.fillAmount = luongMauHienTai/luongMauToiDa;
+        //use to caculate the amount of health when the player get hit
+    
     }
 }
